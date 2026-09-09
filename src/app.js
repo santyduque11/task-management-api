3057df54ae25f8330795444b198bbcab8ac5d7f2
Santiago Llano Duque <santyduque112266@gmail.com>
2026-09-08 19:07:59 -0500

    feat: add cors security middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swagger");
 
@@ -12,6 +13,8 @@ const errorHandler = require("./middleware/errorHandler");
 const app = express();
 
 app.use(helmet());
+
+app.use(cors());
 
 app.use(express.json());
 
